e600b67041aefb29753da99833a77e1add0abd96
notyumin <tan_yu_min@s2015.sst.edu.sg>
2022-08-27 05:10:37 +0800

    Implement basic search

---
+++ static/js/script.js
@@ -0,0 +1,89 @@
+var $results, pagesIndex
+
+// Retrieve index file
+function initLunr() {
+  // First retrieve the index file
+  $.getJSON("js/lunr/PagesIndex.json")
+    .done(function (index) {
+      pagesIndex = index
+      console.log("index:", pagesIndex)
+    })
+    .fail(function (jqxhr, textStatus, error) {
+      var err = textStatus + ", " + error
+      console.error("Error getting Hugo index file:", err)
+    })
+}
+
+// Hook up listener to the input field
+function initUI() {
+  $results = $("#results")
+
+  $("#search").keyup(function () {
+    $results.empty()
+
+    var query = $(this).val()
+    var results
+    /*if () {
+      //if query is number
+      var results = numberSearch(query)
+    } else {*/
+    //if query is words
+    results = search(query)
+    /*}*/
+
+    renderResults(results)
+  })
+}
+
+/**
+ * Trigger a search and transform the result
+ *
+ * @param  {String} query
+ * @return {Array}  results
+ */
+function search(query) {
+  return pagesIndex.filter((page) => {
+    return (
+      page.title.toLowerCase().search(query.toLowerCase()) != -1 ||
+      page.content.toLowerCase().search(query.toLowerCase()) != -1 ||
+      page.href.replace("/hymns/", "").search(query) != -1
+    )
+  })
+}
+
+/**
+ * Display the 10 first results
+ *
+ * @param  {Array} results to display
+ */
+function renderResults(results) {
+  if (!results.length) {
+    return
+  }
+
+  // Only show the ten first results
+  results.slice(0, 10).forEach(function (result) {
+    var $result = $("<li class='box'>")
+    $result.append(
+      $("<a>", {
+        href: result.href,
+        text: result.href.replace("/hymns/", "") + " " + result.title,
+        class: "title is-4",
+      })
+    )
+
+    $results.append($result)
+  })
+}
+
+// Let's get started
+initLunr()
+
+$(document).ready(function () {
+  initUI()
+})
+
+//pre-load full list
+$(window).on("load", () => {
+  renderResults(pagesIndex)
+})
