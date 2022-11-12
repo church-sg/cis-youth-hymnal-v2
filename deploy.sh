33753def2473a0f70f84ab7c37663b24dc0442db
notyumin <tan_yu_min@s2015.sst.edu.sg>
2022-11-13 01:26:55 +0800

    New deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,14 @@
 # Build using Hugo
-hugo
 
 STATUS="$(git status)"
 
 if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
 then
-    # remove public directory from gitignore
-    sed -i "" '/public/d' ./.gitignore
-    git add .
-    git commit -m "Edit .gitignore to publish"
-
-    # push "public" to gh-pages branch
+    hugo # build using hugo
+    git add public/ -f # force add public/ (cuz it's ignored)
+    git commit -m "Commit to deploy"
     git push origin `git subtree split --prefix public main`:gh-pages --force
     git reset HEAD~
-    git checkout .gitignore
 else
     echo "Need clean working directory to publish"
 fi
