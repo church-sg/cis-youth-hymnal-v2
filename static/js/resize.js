b0a07cdf6b981b157b3993ecb82bd6b8e406c886
notyumin <tan_yu_min@s2015.sst.edu.sg>
2022-11-13 11:15:52 +0800

    Fix for window.innerheight returning wrong values

--- static/js/resize.js
+++ static/js/resize.js
@@ -1,8 +1,19 @@
 // https://dev.to/nirazanbasnet/dont-use-100vh-for-mobile-responsive-3o97
 
-const documentHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty("--vh", `${window.innerHeight}px`)
-}
-window.addEventListener("resize", documentHeight)
-documentHeight()
+const setDocHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--vh", `${window.innerHeight}px`);
+
+  // show content after size is set
+  const content = document.getElementsByClassName("content")[0];
+  if (content.style.visibility == "hidden") {
+    content.style.visibility = "visible";
+  }
+};
+
+window.addEventListener("resize", setDocHeight);
+
+// hide content before size is set because otherwise there are ugly artifacts
+document.getElementsByClassName("content")[0].style.visibility = "hidden";
+// window.innerHeight bug: https://bugzilla.mozilla.org/show_bug.cgi?id=771575
+setTimeout(setDocHeight, 50); // Minimum seems to be 20, but 50 just to be safe
