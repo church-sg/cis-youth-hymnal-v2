63eaaabca75c6fcf40595fd19a52f47d396c5bf9
notyumin <tan_yu_min@s2015.sst.edu.sg>
2022-11-15 20:30:44 +0800

    Update DEVELOPMENT.md and add comment to script.js

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -4,11 +4,67 @@
 
 - [Development](#development)
   - [Table of Contents](#table-of-contents)
+  - [Codebase Walkthrough](#codebase-walkthrough)
+    - [Hymns](#hymns)
+    - [Layouts](#layouts)
+    - [Static Files](#static-files)
+    - [Scripts](#scripts)
   - [Deployment](#deployment)
     - [New Hymns](#new-hymns)
       - [1. Add to index](#1-add-to-index)
       - [2. Add to cache](#2-add-to-cache)
     - [Deploy](#deploy)
+
+## Codebase Walkthrough
+
+### Hymns
+
+The hymn lyrics can be found in the [content directory](./content/hymns/).
+They are named according to their hymn number (e.g. hymn number 200 would be `200.md`).
+
+At the top of each file, there is a block of text enclosed in 3 dashes (`---`) known as "frontmatter".
+
+```
+---
+title: You Are My Strength
+medleyFrom: 0
+medleyTo: 0
+---
+```
+
+The frontmatter contains metadata of each file, which in our case are the hymn's title, and medleys.
+
+Another important thing to know is that each line in the hymn needs to end with 2 spaces.
+This is to let Markdown know to start a new line.
+
+```
+...
+**Verse 1**  <- 2 spaces!
+You are my strength when I am weak  <- 2 spaces!
+You are the treasure that I seek  <- 2 spaces!
+You are my All in All.
+...
+```
+
+### Layouts
+
+The HTML layouts can be found in the [layouts directory](./layouts/).
+The layouts are written in HTMl, with [Hugo's templating syntax](https://gohugo.io/templates/introduction/).
+
+### Static Files
+
+Static files such as icons, CSS and scripts can be found in the [static directory](./static/).
+
+### Scripts
+
+The few important scripts used are:
+
+- [Gruntfile.js](./Gruntfile.js)
+- [generateCache.js](./generateCache.js)
+- [deploy.sh](./deploy.sh)
+
+These are all used for deployment, and will be explained further in the
+[deployment section below](#deployment)
 
 ## Deployment
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,38 +1,38 @@
-var $results, pagesIndex
+var $results, pagesIndex;
 
 // Retrieve index file
 async function initLunr() {
   // First retrieve the index file
   return $.getJSON("./js/lunr/PagesIndex.json")
-    .done(function (index) {
-      pagesIndex = index
-      console.log("index:", pagesIndex)
+    .done(function(index) {
+      pagesIndex = index;
+      console.log("index:", pagesIndex);
     })
-    .fail(function (jqxhr, textStatus, error) {
-      var err = textStatus + ", " + error
-      console.error("Error getting Hugo index file:", err)
-    })
+    .fail(function(jqxhr, textStatus, error) {
+      var err = textStatus + ", " + error;
+      console.error("Error getting Hugo index file:", err);
+    });
 }
 
 // Hook up listener to the input field
 function initUI() {
-  $results = $("#results")
+  $results = $("#results");
 
-  $("#search").keyup(function () {
-    $results.empty()
+  $("#search").keyup(function() {
+    $results.empty();
 
-    var query = $(this).val()
-    var results
+    var query = $(this).val();
+    var results;
     if (isNaN(parseInt(query))) {
       //if query is words
-      results = search(query)
+      results = search(query);
     } else {
       //if query is number
-      var results = numberSearch(query)
+      var results = numberSearch(query);
     }
 
-    renderResults(results)
-  })
+    renderResults(results);
+  });
 }
 
 /**
@@ -46,30 +46,28 @@ function search(query) {
     return (
       page.title.toLowerCase().search(query.toLowerCase()) != -1 ||
       page.content.toLowerCase().search(query.toLowerCase()) != -1
-    )
-  })
+    );
+  });
 }
 
 function numberSearch(number) {
   // only search hymnNo to avoid searching the verse numbers
   return pagesIndex.filter((page) => {
-    return page.href.replace("/hymns/", "").search(number) != -1
-  })
+    return page.href.replace("/hymns/", "").search(number) != -1;
+  });
 }
 
 /**
- * Display the 10 first results
+ * Display search results
  *
  * @param  {Array} results to display
  */
 function renderResults(results) {
-  if (!results.length) {
-    return
-  }
+  if (!results.length) return;
 
   // Show results
-  results.forEach(function (result) {
-    var $result = $("<li>")
+  results.forEach(function(result) {
+    var $result = $("<li>");
     $result.append(
       $("<a>", {
         href: "." + result.href + "/", //"." to transform href to relative href "./"
@@ -79,10 +77,10 @@ function renderResults(results) {
           "</span> " +
           result.title,
       })
-    )
+    );
 
-    $results.append($result)
-  })
+    $results.append($result);
+  });
 }
 
 async function registerSW() {
@@ -91,26 +89,26 @@ async function registerSW() {
       .register("/cis-youth-hymnal-v2/sw.js", {
         scope: "/cis-youth-hymnal-v2/",
       })
-      .then(function (registration) {
-        console.log("Service Worker Registered")
-      })
+      .then(function(registration) {
+        console.log("Service Worker Registered");
+      });
 
-    navigator.serviceWorker.ready.then(function (registration) {
-      console.log("Service Worker Ready")
-    })
+    navigator.serviceWorker.ready.then(function(registration) {
+      console.log("Service Worker Ready");
+    });
   }
 }
 
-$(document).ready(function () {
-  initUI()
-})
+$(document).ready(function() {
+  initUI();
+});
 
 $(window).on("load", () => {
   initLunr().then(() => {
     //pre-load full list
-    renderResults(pagesIndex)
-  })
-})
+    renderResults(pagesIndex);
+  });
+});
 
 // Add service worker
-registerSW()
+registerSW();
